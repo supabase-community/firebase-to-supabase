3240e97bcbdee2493e86223e3e21f1cd32a042c3
Tim Saunders <tim@sauntimo.org>
2022-02-23 23:58:04 +0000

    Fix firestoreusers2json

--- auth/firestoreusers2json.ts
+++ auth/firestoreusers2json.ts
@@ -4,14 +4,13 @@ import * as admin from "firebase-admin";
 const serviceAccount = require("./firebase-service.json");
 
 try {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com` // "https://PROJECTID.firebaseio.com"
-  });
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: `https://${serviceAccount.project_id}.firebaseio.com` // "https://PROJECTID.firebaseio.com"
+    });
 } catch (e) {}
 
 const args = process.argv.slice(2);
-//let db;
 
 if (args.length < 0) {
     console.log('Usage: node firestoreusers2json.js [<filename.json>] [<batch_size>]');
@@ -22,31 +21,34 @@ if (args.length < 0) {
     main();
 }
 
-const filename = args[0] || './users.json';
-const batch_size = args[1] || '100';
-
 async function main() {
-    fs.writeFileSync(filename, '[', 'utf-8');
-    listUsers();    
+    const filename = args[0] || "./users.json";
+    const batchSizeInput = args[1] || "100";
+    const batchSize = parseInt(batchSizeInput);
+    fs.writeFileSync(filename, "[", "utf-8");
+    listUsers(filename, batchSize);
 }
 
-let count = 0;
-async function listUsers(nextPageToken?: string) {
-        admin.auth().listUsers(parseInt(batch_size), nextPageToken)
-        .then((usersFound: admin.auth.ListUsersResult) => {
-            const users: admin.auth.UserRecord[] = usersFound.users;
-            users.forEach((user: admin.auth.UserRecord) => {
-                fs.appendFileSync(filename, (count > 0 ? ',' : '') + JSON.stringify(user, null, 2), 'utf-8');
-                count++;
-            });
-            if (usersFound.pageToken) {
-                listUsers(usersFound.pageToken);
-            } else {
-                fs.appendFileSync(filename, ']\n', 'utf-8');
-            }
-        }).catch((err) => {
-            console.log('ERROR in listUsers', JSON.stringify(err));
+
+async function listUsers(
+  filename: string,
+  batchSize: number,
+  nextPageToken?: string
+) {
+    let count = 0;
+    admin.auth().listUsers(batchSize, nextPageToken)
+    .then((usersFound: admin.auth.ListUsersResult) => {
+        const users: admin.auth.UserRecord[] = usersFound.users;
+        users.forEach((user: admin.auth.UserRecord) => {
+            fs.appendFileSync(filename, (count > 0 ? ',' : '') + JSON.stringify(user, null, 2), 'utf-8');
+            count++;
         });
+        if (usersFound.pageToken) {
+            listUsers(filename, batchSize, usersFound.pageToken);
+        } else {
+            fs.appendFileSync(filename, ']\n', 'utf-8');
+        }
+    }).catch((err) => {
+        console.log('ERROR in listUsers', JSON.stringify(err));
+    });
 }
-
-
