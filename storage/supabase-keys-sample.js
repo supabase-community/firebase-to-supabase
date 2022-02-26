7da7c1c48058623f805ad96b5eab2f4ac0686f69
Mark Burggraf <markb@mantisbible.com>
2022-02-26 15:31:00 -0800

    updated keys sample

---
+++ storage/supabase-keys-sample.js
@@ -0,0 +1,19 @@
+/************************************************************
+  Enter your Supabase Key and Supabase URL below
+  1. go to https://supabase.io, 
+  2. log into your project
+  3. click "settings" on the left (at the bottom)
+  4. click "API"
+  5. copy "URL" to SUPABASE_URL below
+  6. copy your "API Key" (service key) to SUPABASE_KEY below
+  7. save this file and rename it to "supabase.ts"
+*************************************************************/
+"use strict";
+exports.__esModule = true;
+exports.keys = void 0;
+export const keys = {
+    SUPABASE_URL: 'https://xxxxxxxxxxxxxxxxxxxx.supabase.co',
+    // service key is required below
+    SUPABASE_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  };
+  
